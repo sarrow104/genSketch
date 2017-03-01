cf74cc80da22ab5397eaba7d7dc9d3a3cdec5218
sarrow104 <sarrow104@gmail.com>
2017-03-01 18:13:51 +0800

    add sample translation file

---
+++ template/qt/cmake.tpl/${Target}/src/myapp_zh_CN.ts
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.cpp" line="13"/>
+        <source>Main Window</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="15"/>
+        <source>&amp;Open...</source>
+        <translation>打开(&amp;O)</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="17"/>
+        <source>Open an existing file.</source>
+        <translation>打开已有文件</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="20"/>
+        <source>&amp;Quit</source>
+        <translation>退出(&amp;Q)</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="22"/>
+        <source>Quit Application</source>
+        <translation>退出程序</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="25"/>
+        <location filename="mainwindow.cpp" line="29"/>
+        <source>&amp;File</source>
+        <translation>文件(&amp;F)</translation>
+    </message>
+</context>
+</TS>
