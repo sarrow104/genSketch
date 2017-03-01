a790faac238a5b5ba069816f417d0a0caa6694bc
sarrow104 <sarrow104@gmail.com>
2017-03-01 18:15:30 +0800

    auto load resource file myapp_zh_CN.qm

--- template/qt/cmake.tpl/${Target}/src/main.cpp
+++ template/qt/cmake.tpl/${Target}/src/main.cpp
@@ -1,10 +1,18 @@
 #include <QApplication>
+#include <QTranslator>
 
 #include "mainwindow.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QTranslator translator; 
+    // look up e.g. :/translators/myapp_{Locale}.qm
+    if (translator.load(QLocale(), QLatin1String("myapp"), QLatin1String("_"), QLatin1String(":/translators"))) {
+        a.installTranslator(&translator);
+    }
+
     MainWindow w;
     w.show();
 
